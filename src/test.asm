d07ebf17f21f5fa718f531bc8e9ec2c70bc5ada5
James Rubingh <james@wrive.com>
2009-11-17 16:12:52 -0500

    Initial switch from CVS to git
    Signed-off-by: James Rubingh <james@wrive.com>

---
+++ src/test.asm
@@ -0,0 +1,15 @@
+;test.asm
+
+
+starting:
+  ld a, 3
+  xor a ; xx
+  ld hl, 4
+label1:
+label2:
+  add a,b
+  add a, 15 ;(3*5) no preprocessing yet
+  ret;fuck this
+  ld	b	,	5
+  .db 0, 1, 3, 4, 5, 6, 7, 8
+
