1b71a1c80e7d450fe7faab19b505bd8153d73150
James Rubingh <james@wrive.com>
2009-11-17 19:55:59 -0500

    Fixing some spacing, tab issues
    Signed-off-by: James Rubingh <james@wrive.com>

--- lib/pass.c
+++ lib/pass.c
@@ -1,3 +1,13 @@
+/*  Zasm2 Z80 assembler
+    by James Lee Rubingh
+    james@wrive.com
+
+    Copyright (2007) James Rubingh
+    Released under the GPL v2
+
+    査読お願いします!
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,13 +19,13 @@
 
 int assemble(struct tab_entry *tabroot, FILE *infile)
 {
-	int result = 0;
-	struct instruction *root;
+  int result = 0;
+  struct instruction *root;
 
-	root = pass_first(infile, tabroot);
-	result = pass_second(root);
+  root = pass_first(infile, tabroot);
+  result = pass_second(root);
 
-	return result;
+  return result;
 }
 
 /** Go through the tree of instructions and put in opcodes from the
@@ -23,31 +33,31 @@ int assemble(struct tab_entry *tabroot, FILE *infile)
  */
 int apply_table(struct instruction* root, struct tab_entry* table)
 {
-	struct instruction *tmp_i;
-	struct tab_entry *tab_match;
-	int num_of_instructions=0;
+  struct instruction *tmp_i;
+  struct tab_entry *tab_match;
+  int num_of_instructions=0;
 
-	if (root == NULL)
-		return -1;
+  if (root == NULL)
+    return -1;
 
-	tmp_i = root;
-	while (tmp_i) {
-		tab_match = match_opcode(tmp_i);
-		tmp_i = tmp_i->next;
-		num_of_instructions++;
-	}
-	return num_of_instructions;
+  tmp_i = root;
+  while (tmp_i) {
+    tab_match = match_opcode(tmp_i);
+    tmp_i = tmp_i->next;
+    num_of_instructions++;
+  }
+  return num_of_instructions;
 }
 
 struct tab_entry *match_opcode(struct instruction *instruction) 
 {
-	struct tab_entry *tmp_tab = NULL;
+  struct tab_entry *tmp_tab = NULL;
 
-	printf("%s - ", instruction->mnumonic);
-	printf("%ul\n", instruction->opcode);
+  printf("%s - ", instruction->mnumonic);
+  printf("%ul\n", instruction->opcode);
 
 
-	return tmp_tab;
+  return tmp_tab;
 }
 
 /** go through the source file(s) and create a
@@ -56,16 +66,16 @@ struct tab_entry *match_opcode(struct instruction *instruction)
  */
 struct instruction *pass_first(FILE *infile, struct tab_entry *table)
 {
-	struct instruction *root = NULL;
-	label_root = NULL; 
+  struct instruction *root = NULL;
+  label_root = NULL; 
 
-	/** build up the basic tree from the source file(s)
-	 * descends recursively into included source files
-	 */
-	root = parse_source(infile, root);
-	apply_table(root, table);
+  /** build up the basic tree from the source file(s)
+   * descends recursively into included source files
+   */
+  root = parse_source(infile, root);
+  apply_table(root, table);
 
-	return root;
+  return root;
 }
 
 /** Parse text file and create
@@ -81,113 +91,113 @@ struct instruction *pass_first(FILE *infile, struct tab_entry *table)
 struct instruction *parse_source(FILE *infile, struct instruction* initial_root)
 {
 
-	struct instruction *cur_old =NULL, *cur = NULL;
-	struct instruction *inst_root;
-	char buffer[INSTRUCTION_BUFFER_SIZE];
-	char b[INSTRUCTION_BUFFER_SIZE];
-	char *buf, *ptr;
-	int instructions =0;
-	int cur_op_num;
+  struct instruction *cur_old =NULL, *cur = NULL;
+  struct instruction *inst_root;
+  char buffer[INSTRUCTION_BUFFER_SIZE];
+  char b[INSTRUCTION_BUFFER_SIZE];
+  char *buf, *ptr;
+  int instructions =0;
+  int cur_op_num;
 
-	inst_root = initial_root;
-	buf = b;
+  inst_root = initial_root;
+  buf = b;
 
-	while (fgets(buffer, INSTRUCTION_BUFFER_SIZE, infile)) {
-		strip_comment(buffer);
+  while (fgets(buffer, INSTRUCTION_BUFFER_SIZE, infile)) {
+    strip_comment(buffer);
 
-		if (isblank(buffer[0]) || (buffer[0] == '\n')) {
-			/** if the first char is blank, treat as an instruction
-			 * or a blank line.
-			 */
+    if (isblank(buffer[0]) || (buffer[0] == '\n')) {
+      /** if the first char is blank, treat as an instruction
+       * or a blank line.
+       */
 
-			/** split up line, get instruction and operands */
-			if ((buf = (char *) strtok(buffer, whitespace))) {
-				/* keep count of instructions */
-				instructions++;
+      /** split up line, get instruction and operands */
+      if ((buf = (char *) strtok(buffer, whitespace))) {
+        /* keep count of instructions */
+        instructions++;
 
-				cur = (struct instruction *) malloc(sizeof(struct instruction));
-				cur->next = NULL;
-				cur->operands = NULL;
+        cur = (struct instruction *) malloc(sizeof(struct instruction));
+        cur->next = NULL;
+        cur->operands = NULL;
 
-				/* if first element, point root to it */
-				if (!inst_root)
-					inst_root = cur;
+        /* if first element, point root to it */
+        if (!inst_root)
+          inst_root = cur;
 
-				/* attach new link to the end of list */
-				if (cur_old)
-					cur_old->next = cur;
+        /* attach new link to the end of list */
+        if (cur_old)
+          cur_old->next = cur;
 
-				/* convert to uppercase */
-				ptr = buf;
-				do {
-					*ptr = toupper(*ptr);
-				} while (*(ptr++) != '\0');
+        /* convert to uppercase */
+        ptr = buf;
+        do {
+          *ptr = toupper(*ptr);
+        } while (*(ptr++) != '\0');
 
-				strcpy(cur->mnumonic, buf);
+        strcpy(cur->mnumonic, buf);
 
-				/* check for operands */
-				cur_op_num = 0;
-				while ((buf = (char *) strtok(NULL, whitespace))) {
+        /* check for operands */
+        cur_op_num = 0;
+        while ((buf = (char *) strtok(NULL, whitespace))) {
 #ifdef DEBUG
-					printf(":opnd: %s\n", buf);
+          printf(":opnd: %s\n", buf);
 #endif
 
-					if (!cur->operands) 
-						cur->operands = (char **) malloc(sizeof( char *));
-					else 
-						cur->operands = (char **) realloc(cur->operands, (sizeof(char *)*(cur_op_num+1)));
+          if (!cur->operands) 
+            cur->operands = (char **) malloc(sizeof( char *));
+          else 
+            cur->operands = (char **) realloc(cur->operands, (sizeof(char *)*(cur_op_num+1)));
 
-					cur->operands[cur_op_num] = (char *) malloc(strlen(buf) );
-					strcpy(cur->operands[cur_op_num], buf);
+          cur->operands[cur_op_num] = (char *) malloc(strlen(buf) );
+          strcpy(cur->operands[cur_op_num], buf);
 
-					cur->op_num = ++cur_op_num;
-				}
-				cur->op_num = cur_op_num;
-			}
+          cur->op_num = ++cur_op_num;
+        }
+        cur->op_num = cur_op_num;
+      }
 
-			/* set cur_old for next iteration */
-			cur_old = cur;
+      /* set cur_old for next iteration */
+      cur_old = cur;
 
-		} else {
-			/* the first char wasnt blank, so see if it's a label
-			 * or a declaration or whatever
-			 */
-			strip_comment(buffer);
-			if (validate_label(buffer)) 
-				attach_label(buffer, cur);
-		}
+    } else {
+      /* the first char wasnt blank, so see if it's a label
+       * or a declaration or whatever
+       */
+      strip_comment(buffer);
+      if (validate_label(buffer)) 
+        attach_label(buffer, cur);
+    }
 
-	}
+  }
 
-	/* return either the root or the tail */
-	return initial_root == NULL ? inst_root : cur;
+  /* return either the root or the tail */
+  return initial_root == NULL ? inst_root : cur;
 }
 
-/*	add a label_entry to the big list */
+/*  add a label_entry to the big list */
 void attach_label(char *ptr, struct instruction *inst) {
-	struct label_entry *tmp;
+  struct label_entry *tmp;
 
-	tmp = (struct label_entry*) malloc(sizeof(struct label_entry));
-	tmp->instruction = inst;
+  tmp = (struct label_entry*) malloc(sizeof(struct label_entry));
+  tmp->instruction = inst;
 
-	if (label_root == NULL)
-		label_root = tmp;
-	else
-		label_current->next = tmp;
+  if (label_root == NULL)
+    label_root = tmp;
+  else
+    label_current->next = tmp;
 
-	label_current = tmp;
-	tmp->name = (char *)malloc(strlen(ptr) * sizeof(char));
-	strcpy(tmp->name, ptr);
+  label_current = tmp;
+  tmp->name = (char *)malloc(strlen(ptr) * sizeof(char));
+  strcpy(tmp->name, ptr);
 }
 
 /* make sure that the label is a valid label with ascii chars
  * if it's not valid, return a false */
 int validate_label(char *ptr) {
-	while (*(ptr++) != '\0') {
-		if (isalpha(*ptr))
-			return 1;
-	}
-	return 0;
+  while (*(ptr++) != '\0') {
+    if (isalpha(*ptr))
+      return 1;
+  }
+  return 0;
 }
 
 /**  Go back through the instruction tree and using the list of
@@ -195,19 +205,19 @@ int validate_label(char *ptr) {
  */
 int pass_second(struct instruction *root)
 {
-	struct label_entry *cur = NULL;
-	struct instruction *instd = NULL;
+  struct label_entry *cur = NULL;
+  struct instruction *instd = NULL;
 
-	cur = label_root;
-	while (cur) {
-		printf("label: %s  :\n", cur->name);
-		cur = cur->next;
+  cur = label_root;
+  while (cur) {
+    printf("label: %s  :\n", cur->name);
+    cur = cur->next;
 
-		instd = cur->instruction;
-		printf("   inst: %s  :\n", instd->mnumonic);
+    instd = cur->instruction;
+    printf("   inst: %s  :\n", instd->mnumonic);
 
-	}
-	return 0;
+  }
+  return 0;
 }
 
 
--- src/zasm.c
+++ src/zasm.c
@@ -1,9 +1,12 @@
-/*   Zasm2 Z80 assembler
-	 by James Lee Rubingh
-	 james@wrive.com
+/*  Zasm2 Z80 assembler
+    by James Lee Rubingh
+    james@wrive.com
 
-	 査読お願いします!
-	 */
+    Copyright (2007) James Rubingh
+    Released under the GPL v2
+
+    査読お願いします!
+*/
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -24,150 +27,150 @@ struct label_entry *label_current;
 
 void strip_comment(char *ptr)
 {
-	/*! replace any ; or \n with a terminating null
-	  not sure if there is a library function
-	  that does this. Will have to be replaced
-	  eventually if support for unicode is added */
-	while (*ptr != '\0') {
-		if ((*ptr == ';')||(*ptr == '\n')||(*ptr == ':')) {
-			*ptr = '\0';
-			break;
-		}
+  /*! replace any ; or \n with a terminating null
+    not sure if there is a library function
+    that does this. Will have to be replaced
+    eventually if support for unicode is added */
+  while (*ptr != '\0') {
+    if ((*ptr == ';')||(*ptr == '\n')||(*ptr == ':')) {
+      *ptr = '\0';
+      break;
+    }
 
-		ptr++;
-	}
+    ptr++;
+  }
 }
 
 /*! parse the table file and create a list 
   with all of the possible instructions */
 struct tab_entry *read_table(FILE *tabfile) {
-	struct tab_entry *root = NULL;
-	struct tab_entry *temp = NULL;
-	char buffer[TABFILE_BUFFER_SIZE];
-	char hexbuf[TABFILE_BUFFER_SIZE];
-	char *buf, *endptr;
+  struct tab_entry *root = NULL;
+  struct tab_entry *temp = NULL;
+  char buffer[TABFILE_BUFFER_SIZE];
+  char hexbuf[TABFILE_BUFFER_SIZE];
+  char *buf, *endptr;
 
-	while (fgets(buffer, TABFILE_BUFFER_SIZE, tabfile)) {
-		if (buf = (char *) strtok(buffer, tab_whitespace)) {
-			temp = (struct tab_entry *) malloc(sizeof(struct tab_entry));
-			memset(temp, 0, sizeof(struct tab_entry));
-			strcpy(temp->mnumonic, buf);
-			temp->opcode = -1;
-			temp->size = -1;
-			strcpy(hexbuf,"0x");
+  while (fgets(buffer, TABFILE_BUFFER_SIZE, tabfile)) {
+    if (buf = (char *) strtok(buffer, tab_whitespace)) {
+      temp = (struct tab_entry *) malloc(sizeof(struct tab_entry));
+      memset(temp, 0, sizeof(struct tab_entry));
+      strcpy(temp->mnumonic, buf);
+      temp->opcode = -1;
+      temp->size = -1;
+      strcpy(hexbuf,"0x");
 
-			while ((buf = (char *) strtok(NULL, tab_whitespace))) {
-				if (*temp->operands == 0) {
-					strcpy(temp->operands, buf);
-				} else if (*temp->hex_code == 0) {
-					//fill in hex code and opcode
-					strcpy(temp->hex_code, buf);
-					strcpy(hexbuf+2, buf);
-					temp->opcode = (unsigned int)strtol(hexbuf, (char **)NULL, 0);
-				} else if (temp->size == -1){
-					temp->size = atoi(buf);
-				}
-			}
+      while ((buf = (char *) strtok(NULL, tab_whitespace))) {
+        if (*temp->operands == 0) {
+          strcpy(temp->operands, buf);
+        } else if (*temp->hex_code == 0) {
+          //fill in hex code and opcode
+          strcpy(temp->hex_code, buf);
+          strcpy(hexbuf+2, buf);
+          temp->opcode = (unsigned int)strtol(hexbuf, (char **)NULL, 0);
+        } else if (temp->size == -1){
+          temp->size = atoi(buf);
+        }
+      }
 
 #ifdef DEBUG
-			printf("mnumonic: %s\n operand: %s\n hexcode: %s\n opcode: %d\n size: %d\n",
-					temp->mnumonic, temp->operands, temp->hex_code, temp->opcode, temp->size);
+      printf("mnumonic: %s\n operand: %s\n hexcode: %s\n opcode: %d\n size: %d\n",
+          temp->mnumonic, temp->operands, temp->hex_code, temp->opcode, temp->size);
 #endif
-		}
-	}
+    }
+  }
 #ifdef DEBUG
-			puts("Done exporting Table List");
+      puts("Done exporting Table List");
 #endif
 
-	return root;
+  return root;
 }
 
 
 
 int main(int ac, char **av)
 {
-	int c;
-	FILE *infile=NULL, *outfile=NULL, *tabfile=NULL;
-	struct instruction *root=NULL;
-	struct tab_entry *tab_root=NULL;
+  int c;
+  FILE *infile=NULL, *outfile=NULL, *tabfile=NULL;
+  struct instruction *root=NULL;
+  struct tab_entry *tab_root=NULL;
 
-	while (1) {
-		int option_index=0;
-		static struct option long_options[] = {
-			{"version", 0, 0, 'V'},
-			{"tabfile", 1, 0, 'T'},
-			{"verbose", 0, 0, 'v'},
-			{"help", 0, 0, 'h'},
-			{"tiprog", 0, 0, 't'},
-			{"tistrng", 0, 0, 's'},
-			{0, 0, 0, 0}
-		};
+  while (1) {
+    int option_index=0;
+    static struct option long_options[] = {
+      {"version", 0, 0, 'V'},
+      {"tabfile", 1, 0, 'T'},
+      {"verbose", 0, 0, 'v'},
+      {"help", 0, 0, 'h'},
+      {"tiprog", 0, 0, 't'},
+      {"tistrng", 0, 0, 's'},
+      {0, 0, 0, 0}
+    };
 
-		c = getopt_long (ac, av, "vVdtsT:hp",
-				long_options, &option_index);
-		if (c == -1)
-			break;
+    c = getopt_long (ac, av, "vVdtsT:hp",
+        long_options, &option_index);
+    if (c == -1)
+      break;
 
-		switch (c) {
-			case 'h':
-			case 'V':
-				show_help(0);
-				break;
-			case 'v':
-				verbose = 420;
-				break;
-			case 's':
-				tistring = 420;
-				break;
-			case 'T':
-				/* set up table file handle */
-				if (!(tabfile = fopen(optarg, "r"))) {
-					printf("Error opening tabfile: %s\n", optarg);
-					exit(1);
-				}
-				break;
-			default:
-				/* invalid switch, so failing */
-				exit(1);
-		}
-	}
+    switch (c) {
+      case 'h':
+      case 'V':
+        show_help(0);
+        break;
+      case 'v':
+        verbose = 420;
+        break;
+      case 's':
+        tistring = 420;
+        break;
+      case 'T':
+        /* set up table file handle */
+        if (!(tabfile = fopen(optarg, "r"))) {
+          printf("Error opening tabfile: %s\n", optarg);
+          exit(1);
+        }
+        break;
+      default:
+        /* invalid switch, so failing */
+        exit(1);
+    }
+  }
 
-	/* check if tabflie is open, if not use default */
-	if (!tabfile){
-		if (tabfile=fopen("./TASM80.TAB","r")){
-			if (verbose)
-				puts("using default tab file TASM80.TAB");
-		} else {
-			puts("Can't open TASM80.TAB file");
-			exit(1);
-		}
-	}
+  /* check if tabflie is open, if not use default */
+  if (!tabfile){
+    if (tabfile=fopen("./TASM80.TAB","r")){
+      if (verbose)
+        puts("using default tab file TASM80.TAB");
+    } else {
+      puts("Can't open TASM80.TAB file");
+      exit(1);
+    }
+  }
 
-	if (!(infile = fopen(av[optind], "r"))) {
-		printf("error: with source file \"%s\"\n", av[optind]);
-		show_help(1);
-		exit(1);
-	}
+  if (!(infile = fopen(av[optind], "r"))) {
+    printf("error: with source file \"%s\"\n", av[optind]);
+    show_help(1);
+    exit(1);
+  }
 
-	if (!(outfile = fopen(av[optind+1], "w"))) {
-		printf("error: with output file %s\n", av[optind+1]);
-		show_help(1);
-		exit(1);
-	}
+  if (!(outfile = fopen(av[optind+1], "w"))) {
+    printf("error: with output file %s\n", av[optind+1]);
+    show_help(1);
+    exit(1);
+  }
 
-	if (verbose) {
-		printf("infile:  %s\n", av[optind]);
-		printf("outfile: %s\n", av[optind+1]);
-	}
+  if (verbose) {
+    printf("infile:  %s\n", av[optind]);
+    printf("outfile: %s\n", av[optind+1]);
+  }
 
-	/* read table file */
-	tab_root = read_table(tabfile);
+  /* read table file */
+  tab_root = read_table(tabfile);
 
-	/* Do two passes through the code, get a binary from
-	   the assembly code, and a linked list of the program
-	   instructions. */
-	assemble(tab_root, infile);
+  /* Do two passes through the code, get a binary from
+     the assembly code, and a linked list of the program
+     instructions. */
+  assemble(tab_root, infile);
 
-	return 0;
+  return 0;
 }
 
